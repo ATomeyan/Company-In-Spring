16ec8dec1027142bd8e9bfc065e44f4f9d2bcc80
Artur Tomeyan <tomeyan.artur@gmail.com>
2022-04-11 00:14:46 +0400

    Initial

--- src/main/java/com/company/repository/AttendanceRecordRepository.java
+++ src/main/java/com/company/repository/AttendanceRecordRepository.java
@@ -2,11 +2,16 @@ package com.company.repository;
 
 import com.company.entity.AttendanceRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface AttendanceRecordRepository extends JpaRepository<AttendanceRecord, Integer> {
 
-    List<AttendanceRecord> findRecordByEntranceTimeAndDepartmentName(LocalDate date, String departmentName);
+    @Query("select a from AttendanceRecord a join Department d where a.entranceTime = :date and d.name = :name")
+    List<AttendanceRecord> findRecordByEntranceTimeAndDepartmentName(
+            @Param("date") LocalDate date,
+            @Param("name") String departmentName);
 }
