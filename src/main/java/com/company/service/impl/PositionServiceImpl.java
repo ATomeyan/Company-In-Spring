cb137b6ee1cee6ae82116f167025ddfc07fb076d
arturto <tomeyan.artur@gmail.com>
2022-04-14 15:47:58 +0400

    Initial

--- src/main/java/com/company/service/impl/PositionServiceImpl.java
+++ src/main/java/com/company/service/impl/PositionServiceImpl.java
@@ -1,9 +1,14 @@
 package com.company.service.impl;
 
+import ch.qos.logback.classic.Logger;
 import com.company.dto.PositionDto;
 import com.company.entity.Position;
+import com.company.exceptions.NotFoundException;
+import com.company.exceptions.NotValidException;
+import com.company.mapper.PositionMapper;
 import com.company.repository.PositionRepository;
 import com.company.service.PositionService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,27 +18,47 @@ import java.util.List;
 @Service
 public class PositionServiceImpl implements PositionService {
 
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(PositionServiceImpl.class);
+    private final PositionMapper positionMapper;
     private final PositionRepository repository;
 
     @Autowired
     public PositionServiceImpl(PositionRepository repository) {
         this.repository = repository;
+        positionMapper = new PositionMapper();
     }
 
     @Override
     public List<PositionDto> getAllPosition() {
-        List<Position> positions = new ArrayList<>();
-        Iterable<Position> iterable = repository.findAll();
 
-        for (Position p : iterable) {
-            positions.add(p);
-        }
-
-        return positions;
+        return getPositionDto(repository.findAll());
     }
 
     @Override
     public PositionDto getPositionByName(String name) {
-        return repository.findPositionByName(name);
+        if (name == null || name.isEmpty()){
+            LOGGER.error("Position name is not valid {}.", name);
+            throw new NotValidException("Position name is not valid.");
+        }
+
+        Position position = repository.findPositionByName(name).orElse(null);
+        if (position == null){
+            LOGGER.error("Position by name {} is not found.", name);
+            throw new NotFoundException("Position is not found.");
+        }
+
+        return positionMapper.entityToDto(position);
+    }
+
+    private List<PositionDto> getPositionDto(List<Position> position) {
+
+        List<PositionDto> positions = new ArrayList<>();
+
+        for (Position p : position) {
+            PositionDto positionDto = positionMapper.entityToDto(p);
+            positions.add(positionDto);
+        }
+
+        return positions;
     }
 }
