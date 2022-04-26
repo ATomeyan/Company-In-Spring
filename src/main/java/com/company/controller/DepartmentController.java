fca6720f2b9899e6bbd638dccb3b9b9668428fda
arturto <tomeyan.artur@gmail.com>
2022-04-26 11:18:22 +0400

    Initial

--- src/main/java/com/company/controller/DepartmentController.java
+++ src/main/java/com/company/controller/DepartmentController.java
@@ -1,12 +1,39 @@
 package com.company.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.company.dto.DepartmentDto;
+import com.company.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/departments")
 public class DepartmentController {
 
+    private final DepartmentService service;
+
+    @Autowired
+    public DepartmentController(DepartmentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentDto>> getDepartments() {
+
+        List<DepartmentDto> dto = service.getAllDepartments();
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<DepartmentDto> getDepartmentByName(@PathVariable String name) {
+
+        DepartmentDto dto = service.getByDepartmentName(name);
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
 }
---
+++ src/main/java/com/company/controller/PositionController.java
@@ -0,0 +1,41 @@
+package com.company.controller;
+
+import com.company.dto.PositionDto;
+import com.company.service.PositionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/positions")
+public class PositionController {
+
+    private final PositionService service;
+
+    @Autowired
+    public PositionController(PositionService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PositionDto>> getPositions() {
+
+        List<PositionDto> dto = service.getAllPosition();
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<PositionDto> getPositionByName(@PathVariable String name) {
+
+        PositionDto dto = service.getPositionByName(name);
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+}
