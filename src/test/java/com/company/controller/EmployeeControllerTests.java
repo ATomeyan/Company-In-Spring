8e8adaf61b445ab9d3d007f0e89f1967a17ed7b0
arturto <tomeyan.artur@gmail.com>
2022-09-01 18:58:12 +0400

    Initial pushing tests

---
+++ src/test/java/com/company/controller/EmployeeControllerTests.java
@@ -0,0 +1,71 @@
+package com.company.controller;
+
+import com.company.dto.EmployeeDto;
+import com.company.entity.Employee;
+import javafx.application.Application;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author Artur Tomeyan
+ * @date 01/09/2022
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+class EmployeeControllerTests {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    private String getUrl() {
+        return "http://localhost:" + port;
+    }
+
+    @Test
+    void getAllEmployees() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(null, httpHeaders);
+
+        ResponseEntity<String> response = testRestTemplate.exchange(getUrl() + "/employees", HttpMethod.GET,
+                httpEntity, String.class);
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    void addEmployee() {
+        Employee employee = new Employee();
+
+        employee.setFirstName("Armen");
+        employee.setLastName("Yavrumyan");
+        employee.setDateOfBirth(LocalDate.of(1992, 5, 2));
+        employee.setEmail("arm.y@mail.com");
+        employee.setGender("M");
+        employee.setActive(true);
+        employee.setPosition(employee.getPosition());
+        employee.setDepartment(employee.getDepartment());
+
+        ResponseEntity<EmployeeDto> postResponse = testRestTemplate.postForEntity(getUrl() + "/employees", employee, EmployeeDto.class);
+        assertNotNull(postResponse);
+        assertNotNull(postResponse.getBody());
+    }
+}
---
+++ src/test/java/com/company/service/EmployeeServiceTests.java
@@ -0,0 +1,11 @@
+package com.company.service;
+
+import com.company.repository.EmployeeRepository;
+
+/**
+ * @author Artur Tomeyan
+ * @date 01/09/2022
+ */
+public class EmployeeServiceTests {
+
+}
