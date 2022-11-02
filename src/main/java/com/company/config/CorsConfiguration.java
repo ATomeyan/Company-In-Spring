948d589bbf1a65650afd4781ae29771725af5e06
arturto <tomeyan.artur@gmail.com>
2022-11-02 17:06:37 +0400

    Initial

--- src/main/java/com/company/config/CorsConfiguration.java
+++ src/main/java/com/company/config/CorsConfiguration.java
@@ -12,8 +12,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").
-                allowedOrigins(url).
-                allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedOrigins(url)
+                .allowedMethods("*");
     }
 }
